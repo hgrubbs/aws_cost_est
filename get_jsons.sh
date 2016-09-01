089efbe1de3d93606976c8039433fa7da8f48682
hgrubbs <hunter.grubbs@gmail.com>
2016-09-01 14:08:31 -0500

    get_jsons.sh

---
+++ get_jsons.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "getting instances"
+aws ec2 describe-instances --output json > instances.json
+
+echo "getting volumes"
+aws ec2 describe-volumes --output json > volumes.json
+
+echo "getting snapshots"
+aws ec2 describe-snapshots --filter Name="description",Values="*dc3-srv*" --output json > volumes.json
